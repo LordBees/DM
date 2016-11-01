a35633673345e835a6b973a5a4f5789d71b10f4b
LordBees <LordBees@users.noreply.github.com>
2016-11-01 16:18:21 +0000

    Add files via upload
    saved file

--- main/io test.py
+++ main/io test.py
@@ -15,3 +15,28 @@ def readfile(fname):
 x =  readfile('Dwarf.txt')
 for y in range(len(x)):
     x[y] = x[y].strip('\n')
+
+
+data = x
+x=None
+test_in = ['Dwarf','HILL']
+##readingdata = [False,'',0]
+##for x in range(len(data)):
+##    if '[' in data[x]:
+##        if not readingdata[0]:##define cases for where data goes
+##            if data[x] == '[BASE]':##base char attributes
+##                pass
+##            elif data[x]
+##            readingdata[2] == data[x+1]##stores linelength
+reading = False
+
+ctag = ''
+for x in range(len(data)):
+    #detets start and end of file
+    if '[' in data[x]:
+        ctag  = data[x]
+        reading = True
+    elif data[x] == '[END]':
+        reading = False
+        
+        
