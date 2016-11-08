34e030d7c7bd779e06018fb2db24143f2f3d6910
LordBees <LordBees@users.noreply.github.com>
2016-11-08 15:46:34 +0000

    NEAR FINAL
    added saving support for preset characters
    need to finish some stats generation on preset chars
    double check needed in code
    comments needed in code

--- DM.py
+++ DM.py
@@ -2006,7 +2006,29 @@ class createcharwin:#(main_win):##better to create new window form,from scratch
                     temp = []
                     for x in range(len(B_RM)):#B_RM and P_RM are both same length
                         temp.append(int(B_RM[x])+int(P_RM[x]))#add them together
-                    print(temp)  
+                    print(temp)
+                    ARRAY_PATTR_RMOD=[]
+                    ARRAY_PATTR_RMOD=temp
+                    temp = []
+
+                    P_ST = self.get_primaryattributes_BTN()
+                    for x in range(len(P_ST)):#split and replace with just number part
+                        P_ST[x] = P_ST[x].split('|')[1]
+                    ARRAY_PATTR=[]
+                    ARRAY_PATTR = P_ST
+
+##                    S_ST = self.get_secondaryskills()
+##                    temp = []
+##                    for x in range(len(S_ST[0])):##both should be same length
+##                        temp.append((S_ST[0][x],S_ST[1][x]))
+##                    ARRAY_SATTR=[]
+##                    ARRAY_SATTR = temp
+##                    temp = []
+                    ARRAY_SKILLS = self.get_secondaryskills()
+
+                    ARRAY_SAVES = self.get_savingthrows()
+
+                    ARRAY_HPMISC = self.get_hpmiscskills()
                     #input()
                     #rolling other dice for misc
                         #initiative
@@ -2015,17 +2037,33 @@ class createcharwin:#(main_win):##better to create new window form,from scratch
                     
                     
                     #primary info box
-                    yu=[self.charbase_name_CHR_BOX_VAR.get(),#char name
+                    ARRAY_NAMESTAT=[self.charbase_name_CHR_BOX_VAR.get(),#char name
                      self.charbase_name_IRL_BOX_VAR.get(),#irl name
                      self.Backgrounds_setup_CBO_CURR_LBL_VAR.get(),#background
                      'level',#lvl
                      'faction',#faction
                      str(self.Backgrounds_setup_CBS_CURR_LBL_VAR.get())+' '+str(self.Backgrounds_setup_CBG_CURR_LBL_VAR.get()),#race [subrace race]
                      '0XP']#xp
-                    print(yu)
-                    input()
-                    
+                    #print('NAMeSTAT\n',yu)
+                    #input()
+
+                    TOSAVE = [ARRAY_NAMESTAT,ARRAY_PATTR,ARRAY_PATTR_RMOD,'PERCEPTION','INSPIRATION','PROFICIENCY BONUS',ARRAY_SAVES,ARRAY_SKILLS,ARRAY_HPMISC,
+                              [self.Backgrounds_hitdie_HTD_BOX_VAR.get(), 'TOT', 0, 0, 0, 0, 0, 0],[[[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']],' '],
+                              self.get_Backgrounds_LANG_TRT_TXT(),self.get_Backgrounds_misctraits_inventory_INV_TXT(),
+                              [self.get_Backgrounds_misctraits_DPT_TXT(),self.get_Backgrounds_misctraits_DPI_TXT(),self.get_Backgrounds_misctraits_DPB_TXT(),self.get_Backgrounds_misctraits_DPF_TXT(),
+                               self.get_Backgrounds_traits_TRT_TXT()]]
+                    print('\n\nsave{}>')
+                    print(TOSAVE)
                     ##saving data
+
+                    Fs = MEGA.mega2(saveloc[0])
+
+                    TOSAVE = self.internal_prepsave(TOSAVE)
+                    for x in range(len(TOSAVE)-1):
+                        print(TOSAVE[x],end='')
+                        Fs.adddata(TOSAVE[x])
+                        print('Done')
+                    Fs.save()
                     
                     #finally close window
                     messagebox.showinfo('test','save complete')
@@ -2041,7 +2079,7 @@ class createcharwin:#(main_win):##better to create new window form,from scratch
             self.This_win.destroy()##destroys current window instance then creates a new instance of the window
             createcharwin()
     ##get
-    def get_primaryattributes_BTN(self):##get button content
+    def get_primaryattributes_BTN(self):##get button content:primary stats
         return[
             self.primaryattributes_STR_BTN_VAR.get(),
             self.primaryattributes_DEX_BTN_VAR.get(),
@@ -2050,6 +2088,7 @@ class createcharwin:#(main_win):##better to create new window form,from scratch
             self.primaryattributes_WIS_BTN_VAR.get(),
             self.primaryattributes_CHR_BTN_VAR.get()
             ]
+    
     def get_primaryattributes_LBL(self):##get label content: roll modifier for primary stats
         return[
             self.primaryattributes_STR_LBL_VAR.get(),
@@ -2122,6 +2161,15 @@ class createcharwin:#(main_win):##better to create new window form,from scratch
             self.secondaryskills_SOH_CHK_VAR.get(),
             self.secondaryskills_STE_CHK_VAR.get(),
             self.secondaryskills_SRV_CHK_VAR.get()]]
+    
+    def get_hpmiscskills(self):
+        ##hpmiscskills
+        return[self.HPmiscskills_AMC_BOX_VAR.get(),
+               self.HPmiscskills_INI_BOX_VAR.get(),
+               self.HPmiscskills_SPD_BOX_VAR.get(),
+               self.HPmiscskills_HMX_BOX_VAR.get(),
+               self.HPmiscskills_HTP_BOX_VAR.get(),
+               self.HPmiscskills_HCR_BOX_VAR.get()]
     
     def get_Backgrounds_misctraits_DPT_TXT(self):
         return self.Backgrounds_misctraits_DPT_TXT.get(1.0,'end-1c')
@@ -2893,7 +2941,160 @@ class createcharwin:#(main_win):##better to create new window form,from scratch
                     self.set_Backgrounds_misctraits_DPF_TXT(data[x][bgid])
                 
     
-    
+    ##mega file save prep
+    def internal_prepsave(self,GotData):##handles conversion for data to save in megafile
+        ##doublecheck this vs
+        Fn = ['charbase',
+              'primary',
+              'rollmod',
+              'perception',
+              'inspiration',
+              'profbonus',
+              'savingthrows',
+              'secondary',
+              'hpmisc',
+              'dicesavemisc',
+              'attackspells',
+              'attackspells_text',
+              'langskills',
+              'equipmain',
+              'pinfo_base',
+              'pinfo_ideals',
+              'pinfo_bonds',
+              'pinfo_flaws',
+              'pinfo_addditfeatures']
+        print(len(Fn))
+        print(Fn,'\n')
+        dat2rt = ['']*20
+        temp = []
+        
+        ##1
+        print(GotData[0])
+        for x in range(len(GotData[0])):
+            GotData[0][x] = GotData[0][x].strip('\n')
+            if GotData[0][x] == '':##add spaces for csvising 
+                GotData[0][x] = ' '
+        dat2rt[0] = [Fn[0],[self.array2csv(GotData[0])]]
+        print(dat2rt[0])
+        
+        ##2
+        print(GotData[1])
+        for x in range(len(GotData[1])):
+            print(GotData[1][x])
+            if GotData[1][x] == '':
+                GotData[1][x] = ' '
+        dat2rt[1] = [Fn[1],[self.array2csv(GotData[1])]]
+        print(dat2rt[1])
+        
+        #3
+        print(GotData[2])
+        for x in range(len(GotData[2])):
+            if GotData[2][x] == '':
+                GotData[2][x] = ' ' 
+        dat2rt[2] = [Fn[2],[self.array2csv(GotData[2])]]
+        
+##        #4,5,6
+##        #temp = [GotData[3],GotData[5],GotData[6]]
+##        dat2rt[3] = [Fn[3],[self.array2csv(temp)]]
+##        temp = []
+
+        #4
+        print(GotData[3])
+        if GotData[3] == '':
+            GotData[3] = ' '
+        dat2rt[3] = [Fn[3],[self.array2csv([GotData[3]])]]
+
+        #5
+        if GotData[4] == '':
+            GotData[4] = ' '
+        dat2rt[4] = [Fn[4],[self.array2csv([GotData[4]])]]
+
+        #6
+        if GotData[5] == '':
+            GotData[5] = ' '
+        dat2rt[5] = [Fn[5],[self.array2csv([GotData[5]])]]
+        
+        #7
+        for x in range(len(GotData[6])):#checkbox value is always 1 or 0 so can just apppend as string
+            if GotData[6][1] == '':
+                GotData[6][1] = ' '
+            temp.append(str(GotData[6][x][0])+str(GotData[6][x][1]))
+        dat2rt[6] = [Fn[6],[self.array2csv(temp)]]
+        temp = []
+        
+        #8 a,b
+        for x in range(len(GotData[7][1])):##convert 8b to str from int
+            if GotData[7][0][x] == '':##put here because checkbox list is the same size
+                GotData[7][0][x] = ' '
+            GotData[7][1][x] = str(GotData[7][1][x])
+        dat2rt[7] = [Fn[7],[self.array2csv(GotData[7][0]),self.array2csv(GotData[7][1])]]
+        
+        #9
+        for x in range(len(GotData[8])):##could function this
+            if GotData[8][x] == '':
+                GotData[8][x] = ' ' 
+        dat2rt[8] = [Fn[8],[self.array2csv(GotData[8])]]
+        
+        #10
+        for x in range(len(GotData[9])):
+            GotData[9][x] = str(GotData[9][x])
+            if GotData[9][x] == '':
+                GotData[9][x] = ' ' 
+        dat2rt[9] = [Fn[9],[self.array2csv(GotData[9])]]
+        
+        #11
+        print(GotData[10])
+        for x in range(len(GotData[10][0])):#always same entries length for block
+            if GotData[10][0][0][x] == '':
+                GotData[10][0][0][x] = ' '
+                
+            if GotData[10][0][1][x] == '':
+                GotData[10][0][1][x] = ' '
+                
+            if GotData[10][0][2][x] == '':
+                GotData[10][0][2][x] = ' '
+        dat2rt[10] = [Fn[10], [self.array2csv(GotData[10][0][0]),self.array2csv(GotData[10][0][1]),self.array2csv(GotData[10][0][2])] ]
+        
+        #12
+##              for x in range(len(GotData[7][2])-1):
+##                  if GotData[7][2][x] == '\\'
+        
+        if GotData[10][1] == '':
+                GotData[10][1] = ' '
+        dat2rt[11] = [Fn[11],GotData[10][1].split('\n')]
+        
+        #13
+        if GotData[11] == '':
+                GotData[11] = ' '
+        dat2rt[12] = [Fn[12],GotData[11].split('\n')]
+        
+        #14
+        if GotData[12] == '':
+                GotData[12] = ' '
+        dat2rt[13] = [Fn[13],GotData[12].split('\n')]
+        
+        #15
+        for x in range(len(GotData[13])):##does all 5 below as in a list together
+            if GotData[13][x] == '':
+                GotData[13][x] = ' '
+        dat2rt[14] = [Fn[14],GotData[13][0].split('\n')]
+        
+        #16
+        print('\n\n')
+        print(Fn[16],'\n')
+        print(GotData[13][1].split('\n'))
+        dat2rt[15] = [Fn[15],GotData[13][1].split('\n')]
+        
+        #17
+        dat2rt[16] = [Fn[16],GotData[13][2].split('\n')]
+        
+        #18
+        dat2rt[17] = [Fn[17],GotData[13][3].split('\n')]
+
+        #19
+        dat2rt[18] = [Fn[18],GotData[13][4].split('\n')]
+        
+        return dat2rt#GotData#dat2rt
 ##    ##not used anymore split up within button functions
 ##    def internal_process_backgrounddata(self):#process backgroundform refreshing for character background
 ##        #readingdata = [False,'',0]##reading,reader,linesleft
@@ -3024,6 +3225,7 @@ class createcharwin:#(main_win):##better to create new window form,from scratch
 ####            else:
 ####                #pass
 ####                print('no race selected')
+                    
 ##            
 
             
